ec4ab6c1835fe566851d90387a3124eecda65f91
Alejandro Gullón <agullon@redhat.com>
2023-04-18 14:38:09 +0200

    fix

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,14 +8,8 @@ source $TESTS_PATH/venv/bin/activate
 set -u
 trap 'echo "[ERROR] Unbound variable must be set"' EXIT
 
-TESTING_ENVIRONMENT=${1:-"stage"}
-JUNIT_XML_REPORT=${2:-"junit_report.xml"}
-LOGFILE=${3:-"pytest.log"}
-
-# init env vars
-export OFFLINE_TOKEN=${OFFLINE_TOKEN}
-export ANSIBLE_CONFIG="$TESTS_PATH/ansible.cfg"
-export SSO_BASE_HOST="https://sso.redhat.com/auth/realms/redhat-external"
+# set env vars
+export TESTING_ENVIRONMENT=${TESTING_ENVIRONMENT:-"stage"}
 if [ "${TESTING_ENVIRONMENT}" == "prod" ]; then
     export API_BASE_HOST="https://api.openshift.com"
 elif [ "${TESTING_ENVIRONMENT}" == "stage" ]; then
@@ -25,13 +19,19 @@ else
     exit 1
 fi
 
-# init test config vars
-export KAFKA_NAME=${KAFKA_NAME:-"kafka-instance"}
+export OFFLINE_TOKEN=${OFFLINE_TOKEN}
+export ANSIBLE_CONFIG="$TESTS_PATH/ansible.cfg"
+export SSO_BASE_HOST="https://sso.redhat.com/auth/realms/redhat-external"
+
+export KAFKA_NAME=${KAFKA_NAME:-"kafka-inst-$(openssl rand -hex 4)"}
 export BILLING_MODEL=${BILLING_MODEL:-"standard"}
 export CLOUD_PROVIDER=${CLOUD_PROVIDER:-"aws"}
 export KAFKA_INSTANCE_PLAN=${KAFKA_INSTANCE_PLAN:-"developer.x1"}
 export REGION=${REGION:-"us-east-1"}
 export TOPIC_NAME=${TOPIC_NAME:-"topic-$(openssl rand -hex 4)"}
+
+export JUNIT_XML_REPORT=${JUNIT_XML_REPORT:-"junit_report.xml"}
+export LOGFILE=${LOGFILE:-"pytest.log"}
 
 # run test suite
 pytest "$TESTS_PATH/test_suite/basic_test_suite.py" --junit-xml="$TESTS_PATH/${JUNIT_XML_REPORT}" --log-file="$TESTS_PATH/${LOGFILE}"
